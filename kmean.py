f9d957a2861a6d3afc95eb6cd30e76283f3b00e1
timoniche <dmitriidulaev@gmail.com>
2024-05-03 17:57:40 +0300

    kmeans clustering added

--- kmean.py
+++ kmean.py
@@ -1,6 +1,9 @@
 import pathlib
+import matplotlib.pyplot as plt
 
-from pyspark.ml.feature import VectorAssembler
+from pyspark.ml.clustering import KMeans
+from pyspark.ml.evaluation import ClusteringEvaluator
+from pyspark.ml.feature import VectorAssembler, StandardScaler
 from pyspark.sql import SparkSession
 
 spark = SparkSession.builder \
@@ -37,13 +40,65 @@ def load_dataset(dataset_path):
     print(f'First {show_n} vectorized: ')
     assembled_data.select(output_col).show(show_n)
 
+    return assembled_data
+
+
+def scale_assembled_dataset(assembled_data):
+    scaler = StandardScaler(
+        inputCol='features',
+        outputCol='scaled_features',
+        withStd=True,
+        withMean=False,
+    )
+    scaler_model = scaler.fit(assembled_data)
+    scaled_data = scaler_model.transform(assembled_data)
+
+    scaled_data.select('scaled_features').show(5)
+
+    return scaled_data
+
+
+k_range = range(2, 7)
+
+
+def kmeans_clustering(scaled_data):
+    evaluator = ClusteringEvaluator(
+        predictionCol='prediction',
+        featuresCol='scaled_features',
+        metricName='silhouette',
+        distanceMeasure='squaredEuclidean'
+    )
+
+    silhouette_scores = []
+    for k in k_range:
+        kmeans = KMeans(featuresCol='scaled_features', k=k)
+        model = kmeans.fit(scaled_data)
+        predictions = model.transform(scaled_data)
+        score = evaluator.evaluate(predictions)
+
+        silhouette_scores.append(score)
+        print(f'Silhouette Score for k = {k} is {score}')
+
+    return silhouette_scores
+
+
+def plot_silhouette_scores(scores):
+    plt.plot(k_range, scores)
+    plt.xlabel('k')
+    plt.ylabel('silhouette score')
+    plt.title('Silhouette Score')
+    plt.show()
+
 
 def main():
     curdir = str(pathlib.Path(__file__).parent.resolve())
     dataset_filename = 'small.csv'
     dataset_path = curdir + '/' + dataset_filename
 
-    load_dataset(dataset_path)
+    assembled_data = load_dataset(dataset_path)
+    scaled_data = scale_assembled_dataset(assembled_data)
+    scores = kmeans_clustering(scaled_data)
+    plot_silhouette_scores(scores)
 
 
 if __name__ == '__main__':
